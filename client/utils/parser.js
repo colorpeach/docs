0f4e94c1cf9771e396542191acd69da7875e4bc3
colorpeach <514571352@qq.com>
2014-06-11 15:07:38 +0800

    Update parser.js

--- client/utils/parser.js
+++ client/utils/parser.js
@@ -2,28 +2,35 @@
     var commentMatch = /\/\*\*([\s\S]+?)\*\//g;
     var commentSplit = /\s*\*\s*/g;
     var spaceStr = /^\s*$/;
-    var commentFrag = /(\w+)\s?(?:\{(.+)\})?(.*)/;
-
-    function parser(str){
+    var commentFrag = /(\w+)\s?(?:\{([\s\S]+?)\})?([\s\S]*)/;
+    var preHTML = /<pre>([\s\S]+)<\/pre>/;
+    var parser = function(str){
         var obj = {};
         if(str){
             str.replace(commentMatch,function(){
                 var s = RegExp.$1;
-                var l = s.replace(commentSplit,"").split("@");
+                var l = s.split(/\s*\*\s*@/);
                 var o = {};
                 l.forEach(function(n,i,arr){
                     var key;
                     if(!spaceStr.test(n)){
-                        keys = n.match(commentFrag);
-                        if(keys[1] === "name")
-                            obj[keys[3]] = o;
+                        keys = n.replace(commentSplit,'\n').match(commentFrag);
+                        if(keys){
+                            if(keys[1] === "name")
+                                obj[$.trim(keys[3])] = o;
+                                
+                            if(keys[1] === "description"){
+                                var preMatch = keys[3].match(preHTML)||"";
+                                preMatch && (keys[3] =  "    "+preMatch[1].split(/\s*\n\s*/).join("\n    "));
+                            }
 
-                        o[keys[1]]={value:keys[3]};
+                            o[keys[1]]={value:keys[3]};
 
-                        if(keys[2])
-                            o[keys[1]].type = keys[2];
+                            if(keys[2])
+                                o[keys[1]].type = keys[2];
+                        }
                     }
-                });
+                });   
             });
         }
         return obj;
