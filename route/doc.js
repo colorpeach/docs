368c88cee1d4a635f97c1fa02bb7d0dfe0d9bc83
colorpeach <514571352@qq.com>
2014-06-18 11:54:07 +0800

    Update doc.js

--- route/doc.js
+++ route/doc.js
@@ -2,19 +2,38 @@ var Doc = require('../models/doc');
 var baseRes = require('./baseResponse');
 
 module.exports = function(app){
+    //获取与文档有关联的所有组织
+    app.get('/get/doc/orgs',funtion(req,res){
+        var doc = new Doc(req.body);
+        doc.linkedOrg(function(data){
+            data = (data||[]).map(function(n){return n.org});
+            res.end(baseRes({orgs:data}));
+        });
+    });
 
     //添加文档
     app.post('/post/add/doc',function(req,res){
-
+        req.body.user = req.session.user.login;
+        var doc = new Doc(req.body);
+        doc.save(function(data){
+            res.end(baseRes({doc:data}));
+        });
     });
 
     //删除文档
     app.post('/post/del/doc',function(req,res){
-
+        var doc = new Doc(req.body);
+        doc.del(function(data){
+            res.end(baseRes(data));
+        });
     });
 
     //更新文档
     app.post('/post/update/doc',function(req,res){
-
+        req.body.user = req.session.user.login;
+        var doc = new Doc(req.body);
+        doc.save(function(data){
+            res.end(baseRes({doc:data}));
+        });
     });
 };
