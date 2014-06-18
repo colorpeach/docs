e923b57ea6db05f954ad3edaf35eb47530dfc441
colorpeach <514571352@qq.com>
2014-06-18 22:22:55 +0800

    interface modify

--- db/index.js
+++ db/index.js
@@ -2,6 +2,8 @@ var MongoClient = require('mongodb').MongoClient,
 //     url = 'mongodb://127.0.0.1:27017/docs';
     url = process.env.MONGODB;
 
+console.log(url);
+
 module.exports = {
     connect:function(callback){
         MongoClient.connect(url,callback);
