63f795aab4ac78ba522858f931b53ce85ce293e3
colorpeach <514571352@qq.com>
2014-06-19 22:02:26 +0800

    add modal

--- db/index.js
+++ db/index.js
@@ -1,8 +1,27 @@
-var MongoClient = require('mongodb').MongoClient,
-//     url = 'mongodb://127.0.0.1:27017/docs';
-    url = process.env.MONGODB;
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB || 'mongodb://127.0.0.1:27017/docs';
 
-console.log(url);
+// module.exports = {
+//     connect:function(c,fn){
+//         var self = this;
+//         if(self.db){
+//             var c = db.collection(c);
+//             fn(c);
+//         }else{
+//             MongoClient.connect(url,function(err,db){
+//                 if(err) throw err;
+                
+//                 var c = db.collection(c);
+//                 self.db = db;
+//                 fn(c);
+//             });
+//         }
+//     },
+//     close:function(){
+//         self.db && self.db.close();
+//     }
+//     url:url
+// };
 
 module.exports = {
     connect:function(callback){
