4d93a61f05a48849a05bc44ce69be27d802e5559
colorpeach <514571352@qq.com>
2014-06-25 16:00:56 +0800

    Update user.js

--- route/user.js
+++ route/user.js
@@ -1,5 +1,6 @@
 var user = require('../models/user');
 var doc = require('../models/doc');
+var deck = require('../models/deck');
 var org = require('../models/org');
 var orgUser = require('../models/org.user');
 var orgDoc = require('../models/org.doc');
@@ -17,6 +18,13 @@ module.exports = function(app){
     app.get('/get/user/docs',function(req,res){
         doc.query({user:req.session.user.login},function(list){
             res.end(baseRes({docs:list}));
+        },{content:0});
+    });
+
+    //获取用户创建的幻灯片
+    app.get('/get/user/decks',function(req,res){
+        deck.query({user:req.session.user.login},function(list){
+            res.end(baseRes({decks:list}));
         },{content:0});
     });
 
