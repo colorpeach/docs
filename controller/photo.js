c90247419719994ebf6416d5135ed055ad5f7964
colorpeach <514571352@qq.com>
2014-08-06 02:43:55 +0000

    this is a problem versions

--- controller/photo.js
+++ controller/photo.js
@@ -1,32 +1,26 @@
 var photo = require('../models/photo');
-var user = require('../models/user');
 var baseRes = require('./baseResponse');
 var fs    = require('fs');
 var formidable = require("formidable");
 
 var Photo = {};
+var defaultImg = "iVBORw0KGgoAAAANSUhEUgAAADwAAAA8CAYAAAA6/NlyAAAC40lEQVRoQ+2a147qMBCGJ/QqLhBC9PL+70QHIcQFotfdHaSsIJBN4vzjs2eT3Cae+POMx1NsfHw9FKDHCIH/uLZDDf9xBZMWDS+XSzqdTnS5XN6uZywWo0QiQcViUXy9RYFnsxkdj0e63W6uQCKRCCWTSapUKq6+V/lIDHg4HNpq1GmirPFms+n0mdJ7EeDBYEDX61VpQuagaDRKrVbLl4x3g+HAo9GIzuczZKLxeJwajQZElikECrxYLGi9XkMnmM/nqVQqwWRCgf3sWzsi9H6GAvd6PUKH5oZhUKfT+X0aljBnkxJp1jAN85m73+9hmngUlE6nYWczDHg8Ht+jKYmHo7B6vQ4RDQNGHkdWMuTxFAKr2s1kMrnHzRIPx9e1Wg0iGqbhwDmtwAEHymlxgr9arSB7zE5IoVCAFAgge1jSnM0FQAUfIbCKXUrG0eh4GqJhnpREpvSdtAMzJhiw5D5G7V9eQBgwC2PT3mw2sJyYc+FcLvd7Kx4Mjax6oKsdcA2zwOl0SofDQcX3vYxJpVJUrVYhsr79gUT3EOHA0KUdUWBE5oTMkB5NBOq0HgX70bKUdkX2sAnt55hCHkNWByCmYVWPLeGZtZi0+ZN+v++pe9hut6FeWauG+WdeTFvSlEW99OOqBg7YS70aWX+22xeiTstrbC0RO4vv4fl8fi/XckNctbHG4NwQ5+CjXC5DnRhEw1zT4r4SN8JVIW1N8AueOw/s0BCXXnwBM+hut4N1/J1UyeCZTMYXuBKwblDrQvgB9wyMSAycNOn2vUqC4RqYPe52u3UdNbmdtN/v+G5XNpt1XRVxBYxM6v0C2o13WyxwBEaWbKRgTbluEo8fgREXzKQhrfKdLrTZAv+PsCb8T9BvgSU7gbo0bndN4gVYR9tEF/S7604vwF4Sdl0TV/0PH1nWgsITsI4+r+rkVcd1u92noU/AXpJ11QnoHmetojiew7onKP2/EFh6hf+1/MBp+BPaWEJrznjzpQAAAABJRU5ErkJggg==";
+defaultImg = new Buffer(defaultImg,'base64');
 
 //获取图片流
 Photo.get_img = function(req,res){
     var data = {login : req.query.user};
     photo.query(data,function(list){
         if(list.length){
+            console.log(list[0].stream);
+            var img = list[0].stream;
             var type = list[0].type;
-            fs.readFile(list[0].url,function(err, data){
-                if(err){
-                    console.log(err);
-                }else{
-                    res.writeHead(200,{"Content-Type":type});
-                    data = new Buffer(data.toString('base64'),'base64');
-                    res.write(data,"binary");
-                    res.end();
-                }
-            })
+            res.writeHead(200,{"Content-Type":type});
+            res.write(img,"binary");
+            res.end();
         }else{
-            var data ="iVBORw0KGgoAAAANSUhEUgAAADwAAAA8CAYAAAA6/NlyAAAC40lEQVRoQ+2a147qMBCGJ/QqLhBC9PL+70QHIcQFotfdHaSsIJBN4vzjs2eT3Cae+POMx1NsfHw9FKDHCIH/uLZDDf9xBZMWDS+XSzqdTnS5XN6uZywWo0QiQcViUXy9RYFnsxkdj0e63W6uQCKRCCWTSapUKq6+V/lIDHg4HNpq1GmirPFms+n0mdJ7EeDBYEDX61VpQuagaDRKrVbLl4x3g+HAo9GIzuczZKLxeJwajQZElikECrxYLGi9XkMnmM/nqVQqwWRCgf3sWzsi9H6GAvd6PUKH5oZhUKfT+X0aljBnkxJp1jAN85m73+9hmngUlE6nYWczDHg8Ht+jKYmHo7B6vQ4RDQNGHkdWMuTxFAKr2s1kMrnHzRIPx9e1Wg0iGqbhwDmtwAEHymlxgr9arSB7zE5IoVCAFAgge1jSnM0FQAUfIbCKXUrG0eh4GqJhnpREpvSdtAMzJhiw5D5G7V9eQBgwC2PT3mw2sJyYc+FcLvd7Kx4Mjax6oKsdcA2zwOl0SofDQcX3vYxJpVJUrVYhsr79gUT3EOHA0KUdUWBE5oTMkB5NBOq0HgX70bKUdkX2sAnt55hCHkNWByCmYVWPLeGZtZi0+ZN+v++pe9hut6FeWauG+WdeTFvSlEW99OOqBg7YS70aWX+22xeiTstrbC0RO4vv4fl8fi/XckNctbHG4NwQ5+CjXC5DnRhEw1zT4r4SN8JVIW1N8AueOw/s0BCXXnwBM+hut4N1/J1UyeCZTMYXuBKwblDrQvgB9wyMSAycNOn2vUqC4RqYPe52u3UdNbmdtN/v+G5XNpt1XRVxBYxM6v0C2o13WyxwBEaWbKRgTbluEo8fgREXzKQhrfKdLrTZAv+PsCb8T9BvgSU7gbo0bndN4gVYR9tEF/S7604vwF4Sdl0TV/0PH1nWgsITsI4+r+rkVcd1u92noU/AXpJ11QnoHmetojiew7onKP2/EFh6hf+1/MBp+BPaWEJrznjzpQAAAABJRU5ErkJggg==";
-            data = new Buffer(data,'base64');
             res.writeHead(200,{"Content-Type":"image/png"});
-            res.write(data,"binary");
+            res.write(defaultImg,"binary");
             res.end();
         }
     });
@@ -53,17 +47,6 @@ Photo.post_save_img = function(req,res){
                 photo.update(pic,function(list){
                     res.end(baseRes({successMsg:['修改成功']}));
                 })
-//                photo.query({login:login},function(list){
-//                    if(list.length){
-//                        photo.update(pic,function(data){
-//                            res.end(baseRes({successMsg:['修改成功']}));
-//                        });
-//                    }else{
-//                        photo.save(pic,function(data){
-//                            res.end(baseRes({successMsg:['修改成功']}));
-//                        });
-//                    }
-//                })
             })
         }
     })
